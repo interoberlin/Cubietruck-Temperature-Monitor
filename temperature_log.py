d7fb9e5e23dddeabf100fd3779981d1a6a227458
Matthias <mail@matthiasbock.net>
2015-08-26 22:44:01 +0200

    works

--- temperature_log.py
+++ temperature_log.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python
 
+#
+# To log your cubietruck's temperature
+# add this line to your /etc/crontab:
+#
+# * * * * * root /var/www/temperature_log.py
+#
+
 from datetime import datetime
 
 temp = open("/sys/devices/platform/sunxi-i2c.0/i2c-0/0-0034/temp1_input","r").read().strip()
@@ -10,5 +17,3 @@ now = datetime.now()
 output = str(now.day).zfill(2)+"."+str(now.month).zfill(2)+"."+str(now.year)+"\t"+str(now.hour).zfill(2)+":"+str(now.minute).zfill(2)+"\t"+temp
 
 open("/var/www/temperature.log","a").write(output+"\n")
-
-#print output
